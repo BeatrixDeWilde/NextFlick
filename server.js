d3c6a703287eb0be5bb72ca294430beb6a310677
Beatrix de Wilde <beatrixdewilde@gmail.com>
2015-05-27 16:42:15 +0100

    Split html file into css, html and js file all in folder public. (node script.js does not work)

--- server.js
+++ server.js
@@ -41,6 +41,8 @@ var genreIdLookup = {
 }
 var query_genres = ["Horror","Romance","Thriller"];
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
    res.sendFile(__dirname + '/index.html');
 });
